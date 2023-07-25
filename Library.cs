fe8f285bf454b193e68d272afb39ea3883fb20fc
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2023-07-25 03:40:14 +0300

    Update Library.cs

--- Library.cs
+++ Library.cs
@@ -9,14 +9,14 @@ using System.Security.Cryptography;
 using UnityEngine;
 namespace Library 
 {
-    public class LibraryFunctions
+    public static class LibraryFunctions
     {
-        public float timer;
-        public bool goFor;
+        static float timer;
+        static bool goFor;
         public delegate void SomeDelegate(); 
 
         // Runs 2 Function with given secs,RunFuncWithSecs(givenFunc1,givenFunc2,givenSec)
-        public void RunFuncWithSecs (
+        public static void RunFuncWithSecs (
             SomeDelegate delegate1,
             SomeDelegate delegate2, 
             float Secs
@@ -32,18 +32,17 @@ namespace Library
             else 
                 delegate2();
         }
-        
         // reset all transform values
-        public void ResetAllTransform(this Transform trans) { 
+        public static void ResetAllTransform(this Transform trans) { 
             trans.position = Vector3.zero; // (0,0,0)
             trans.localRotation = Quaternion.identity;
             trans.localScale = new Vector3(1,1,1);
         }
     }
-    public class Encryption //The Encryption Class encrypts and decrypts the string you pass the function
+    public static class Encryption //The Encryption Class encrypts and decrypts the string you pass the function
     {
-        private string hash = "JunkHash@foo"; // Change Here;
-        public string Encrypt(string input) 
+        private static string hash = "JunkHash@foo"; // Change Here;
+        public static string Encrypt(string input) 
         {
             byte[] data = UTF8Encoding.UTF8.GetBytes(input);
             using(MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider()) 
@@ -57,7 +56,7 @@ namespace Library
                 }
             }
         }
-        public string Decrypt(string input) 
+        public static string Decrypt(string input) 
         {
             byte[] data = Convert.FromBase64String(input);
             using(MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider()) 
