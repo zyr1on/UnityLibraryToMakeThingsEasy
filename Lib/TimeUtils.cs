a2128878ae8cc8086eff8f6a82a4a08a26dba41a
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-05-15 14:32:19 +0300

    Update TimeUtils.cs

--- Lib/TimeUtils.cs
+++ Lib/TimeUtils.cs
@@ -4,7 +4,7 @@ public static class TimeUtils {
     static bool goFor;
     public delegate void SomeDelegate(); 
     // Runs 2 Function with given secs,RunFuncWithSecs(givenFunc1,givenFunc2,givenSec)
-    public static void RunFuncWithSecs(SomeDelegate delegate1, SomeDelegate delegate2, float Secs)
+    public static void RunFuncsWithSecs(SomeDelegate delegate1, SomeDelegate delegate2, float Secs)
     {
         timer += Time.deltaTime;
         if (timer >= Secs)
@@ -18,7 +18,7 @@ public static class TimeUtils {
             delegate2();
     }
 
-    public static void RunFunc(SomeDelegate someDelegate,float secs)
+    public static void RunFuncWithSecs(SomeDelegate someDelegate,float secs)
     {
         timer += Time.deltaTime;
         if(timer > secs) {
