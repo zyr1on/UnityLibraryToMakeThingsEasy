3291e19e3ebdbaa1632e6f9399f3032665804129
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-05-15 15:08:22 +0300

    Update TimeUtils.cs

--- Lib/TimeUtils.cs
+++ Lib/TimeUtils.cs
@@ -1,24 +1,50 @@
 using UnityEngine;
-public static class TimeUtils {
-    static float timer = 0.0f;
-    static bool goFor = true;
-    public delegate void SomeDelegate(); 
-    // Runs 2 Function with given secs,RunFuncWithSecs(givenFunc1,givenFunc2,givenSec)
-    public static void RunFuncsWithSecs(SomeDelegate delegate1, SomeDelegate delegate2, float Secs)
-    {
-        timer += Time.deltaTime;
-        if(timer >= Secs) {
-            timer = 0.0f;
-            if(goFor) delegate1();
-            else delegate2();
-            goFor = !goFor;
-        }
-    }
 
-    public static void RunFuncWithSecs(SomeDelegate someDelegate,float secs)
+public static class TimeUtils
+{
+    private static float timer = 0.0f;
+    private static bool goFor = true;
+    private static bool firstCall = true;
+    public enum CallType
     {
+        Immediate,
+        Unimmediate
+    }
+    public delegate void SomeDelegate();
+    public static void RunFuncsWithSecs(SomeDelegate delegate1, SomeDelegate delegate2, float secs, CallType callType = CallType.Unimmediate)
+    {
+        if(callType == CallType.Immediate) {
+            if(firstCall) {
+                delegate1();
+                firstCall = false;
+            }
+            timer += Time.deltaTime;
+            if (timer >= secs) {
+                timer = 0.0f;
+                if (goFor) delegate2();
+                else delegate1();
+                goFor = !goFor;
+            }
+        }
+        else {
+            if (timer >= secs) {
+                timer = 0.0f;
+                if (goFor) delegate1();
+                else delegate2();
+                goFor = !goFor;
+            }
+        }
+        
+    }
+    public static void RunFuncWithSecs(SomeDelegate someDelegate, float secs, CallType callType = CallType.Unimmediate) {
+        if(callType == CallType.Immediate) {
+            if(firstCall) {
+                someDelegate();
+                firstCall = false;
+            }
+        }
         timer += Time.deltaTime;
-        if(timer >= secs) {
+        if (timer >= secs) {
             someDelegate();
             timer = 0.0f;
         }
