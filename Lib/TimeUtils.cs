bc485b59d249f18ef3d97eab97f972f548e93cf4
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-05-15 14:44:16 +0300

    Update TimeUtils.cs

--- Lib/TimeUtils.cs
+++ Lib/TimeUtils.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
 public static class TimeUtils {
-    static float timer = 0;
-    static bool goFor;
+    static float timer = 0.0f;
+    static bool goFor = true;
     public delegate void SomeDelegate(); 
     // Runs 2 Function with given secs,RunFuncWithSecs(givenFunc1,givenFunc2,givenSec)
     public static void RunFuncsWithSecs(SomeDelegate delegate1, SomeDelegate delegate2, float Secs)
     {
+        // after "secs" seconds delegate1 runs and after "secs" seconds delegate2 runs.
         timer += Time.deltaTime;
-        if (timer >= Secs)
-        {
+        if(timer >= Secs) {
             timer = 0.0f;
+            if(goFor) {
+                delegate1();
+            }
+            else {
+                delegate2();
+            }
             goFor = !goFor;
         }
-        if (goFor)
-            delegate1();
-        else
-            delegate2();
     }
 
     public static void RunFuncWithSecs(SomeDelegate someDelegate,float secs)
